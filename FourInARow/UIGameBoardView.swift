1efbb360bb82aac551629c82a6481959f548b0ba
Adland Lee <adland@4mfd.com>
2018-10-01 02:15:33 -0700

    fix base view constraints

--- FourInARow/UIGameBoardView.swift
+++ FourInARow/UIGameBoardView.swift
@@ -43,59 +43,33 @@ class UIGameBoardView: UIView {
     }
     
     func sharedInit() {
-        initViews()
-        updateView()
-    }
-    
-    func initViews() {
         setupViews()
+        updateView()
     }
     
     func updateView() {
         setNeedsDisplay()
     }
+    
     private func setupViews() {
-//        let rows = gameBoard.numberOfRows
-//        let cols = gameBoard.numberOfColumns
-
-//        gameBoardLayout = Array(
-//            repeating: Array(
-//                repeating: GameBoardSlotView(),
-//                count: gameBoard.numberOfColumns
-//            ),
-//            count: gameBoard.numberOfRows
-//        )
-
-//        let count = CGFloat(cols)
-//        slotSize = (frame.size.width - CGFloat((count-1) * spacing)) / CGFloat(count)
-
-//        for r in 0 ..< gameBoard.numberOfRows {
-//            for c in 0 ..< gameBoard.numberOfColumns {
-//                let slotView = GameBoardSlotView()
-//                gameBoardLayout[r][c] = slotView
-//                slotView.row = r
-//                slotView.col = c
-//                addSubview(slotView)
-//            }
-//        }
         
-        translatesAutoresizingMaskIntoConstraints = false
+        translatesAutoresizingMaskIntoConstraints = true
         
-        NSLayoutConstraint.activate([
+        addConstraints([
             widthAnchor.constraint(equalTo: heightAnchor, multiplier: 7/6)
             ])
 
-        let verticalStack = UIStackView()
-        verticalStack.translatesAutoresizingMaskIntoConstraints = false
-        verticalStack.axis = .horizontal
-        verticalStack.distribution = .fillEqually
-        addSubview(verticalStack)
+        let horizontalStack = UIStackView()
+        horizontalStack.translatesAutoresizingMaskIntoConstraints = false
+        horizontalStack.axis = .horizontal
+        horizontalStack.distribution = .fillEqually
+        addSubview(horizontalStack)
 
-        NSLayoutConstraint.activate([
-            verticalStack.topAnchor.constraint(equalTo: topAnchor),
-            verticalStack.trailingAnchor.constraint(equalTo: trailingAnchor),
-            verticalStack.bottomAnchor.constraint(equalTo: bottomAnchor),
-            verticalStack.leadingAnchor.constraint(equalTo: leadingAnchor)
+        addConstraints([
+            horizontalStack.topAnchor.constraint(equalTo: topAnchor),
+            horizontalStack.trailingAnchor.constraint(equalTo: trailingAnchor),
+            horizontalStack.bottomAnchor.constraint(equalTo: bottomAnchor),
+            horizontalStack.leadingAnchor.constraint(equalTo: leadingAnchor)
             ])
 
         
@@ -105,13 +79,13 @@ class UIGameBoardView: UIView {
             column.distribution = .fillEqually
             column.axis = .vertical
             column.tag = c
-            verticalStack.addArrangedSubview(column)
-            for r in 0 ..< rows {
+            horizontalStack.addArrangedSubview(column)
+            for r in stride(from: rows-1, through: 0, by: -1) {
                 let slot = UIGameBoardSlot()
                 slot.translatesAutoresizingMaskIntoConstraints = false
                 slot.tag = r
                 slot.backgroundColor = .gray
-                slot.titleLabel?.text = String(r)
+                slot.setTitle(String(r), for: .normal)
                 column.addArrangedSubview(slot)
             }
         }
@@ -119,19 +93,5 @@ class UIGameBoardView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-//        let rows = gameBoard.numberOfRows
-//        let cols = gameBoard.numberOfColumns
-        
-//        var slotFrame = CGRect(x: 0, y: 0, width: slotSize, height: slotSize)
-        
-//        for r in 0 ..< rows {
-//            for c in 0 ..< cols {
-//                let slotView = gameBoardLayout[r][c]
-//                slotFrame.origin.x = CGFloat(c) * (slotSize + spacing)
-//                slotFrame.origin.y = (CGFloat(rows - 1) - CGFloat(r)) * (slotSize + spacing)
-//                slotView.frame = slotFrame
-//                slotView.textLabel.text = "\(slotView.row!),\(slotView.col!)"
-//            }
-//        }
     }
 }
